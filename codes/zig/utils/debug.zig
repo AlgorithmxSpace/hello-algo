74fc8b3bd4236fb941c5e1119f493f41a58cfce4
MetaSky <creator_meta_sky@163.com>
2025-07-17 21:38:24 +0800

    finish some part of linked_list.

--- codes/zig/utils/debug.zig
+++ codes/zig/utils/debug.zig
@@ -1,13 +1,40 @@
 const std = @import("std");
+const ListNode = @import("ListNode.zig").ListNode;
 
+// 打印数组
 pub fn printArray(comptime T: type, arr: []const T) void {
-    if (arr.len == 0) {
-        std.debug.print("[]", .{});
-        return;
-    }
     std.debug.print("[", .{});
-    for (arr, 0..) |item, i| {
-        std.debug.print("{}{s}", .{ item, if (i == arr.len - 1) "]" else ", " });
+    if (arr.len > 0) {
+        std.debug.print("[", .{});
+        for (arr, 0..) |item, i| {
+            std.debug.print("{}{s}", .{ item, if (i == arr.len - 1) "]" else ", " });
+        }
+    } else {
+        std.debug.print("]", .{});
     }
     std.debug.print("\n", .{});
+}
+
+// 打印列表
+pub fn printList(comptime T: type, list: std.ArrayList(T)) void {
+    std.debug.print("[", .{});
+    if (list.items.len > 0) {
+        for (list.items, 0..) |item, i| {
+            std.debug.print("{}{}", .{ item, if (i == list.items.len - 1) "]" else ", " });
+        }
+    } else {
+        std.debug.print("]", .{});
+    }
+    std.debug.print("\n", .{});
+}
+
+// 打印链表
+pub fn printLinkedList(comptime T: type, node: *const ListNode(T)) void {
+    std.debug.print("{}", .{node.val});
+    if (node.next) |next_node| {
+        std.debug.print("->", .{});
+        printLinkedList(T, next_node); // 采用递归
+    } else {
+        std.debug.print("\n", .{});
+    }
 }
