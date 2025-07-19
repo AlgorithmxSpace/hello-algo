85bafc337f0e0cdf0c11d3f3770f58314d709834
MetaSky <creator_meta_sky@163.com>
2025-07-19 14:17:45 +0800

    update linked list format.

--- codes/zig/utils/debug.zig
+++ codes/zig/utils/debug.zig
@@ -5,20 +5,6 @@
 const std = @import("std");
 const ListNode = @import("ListNode.zig").ListNode;
 
-// 格式化数组
-pub fn printArray(comptime T: type, arr: []const T) void { // todo: - printArray to printSlice
-    std.debug.print("[", .{});
-    if (arr.len > 0) {
-        std.debug.print("[", .{});
-        for (arr, 0..) |item, i| {
-            std.debug.print("{}{s}", .{ item, if (i == arr.len - 1) "]" else ", " });
-        }
-    } else {
-        std.debug.print("]", .{});
-    }
-    std.debug.print("\n", .{});
-}
-
 // 打印列表
 pub fn printList(comptime T: type, list: std.ArrayList(T)) void {
     std.debug.print("[", .{});
@@ -33,15 +19,15 @@ pub fn printList(comptime T: type, list: std.ArrayList(T)) void {
 }
 
 // 打印链表
-pub fn printLinkedList(comptime T: type, node: *const ListNode(T)) void {
-    std.debug.print("{}", .{node.val});
-    if (node.next) |next_node| {
-        std.debug.print("->", .{});
-        printLinkedList(T, next_node); // 采用递归
-    } else {
-        std.debug.print("\n", .{});
-    }
-}
+// pub fn printLinkedList(comptime T: type, node: *const ListNode(T)) void {
+//     std.debug.print("{}", .{node.val});
+//     if (node.next) |next_node| {
+//         std.debug.print("->", .{});
+//         printLinkedList(T, next_node); // 采用递归
+//     } else {
+//         std.debug.print("\n", .{});
+//     }
+// }
 
 pub fn formatSlice(comptime T: type, slice: []const T) SliceFormatter(T) {
     return SliceFormatter(T){ .items = slice };
