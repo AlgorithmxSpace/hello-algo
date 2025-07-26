fc6823a5f1aa970efb20cd02ed2a34adc99d2c40
MetaSky <creator_meta_sky@163.com>
2025-07-26 13:47:23 +0800

    prepare for stack and queue.

--- codes/zig/chapter_stack_and_queue/array_queue.zig
+++ codes/zig/chapter_stack_and_queue/array_queue.zig
@@ -10,12 +10,12 @@ pub fn ArrayQueue(comptime T: type) type {
     return struct {
         const Self = @This();
 
-        nums: []T = undefined,                          // 用于存储队列元素的数组     
-        cap: usize = 0,                                 // 队列容量
-        front: usize = 0,                               // 队首指针，指向队首元素
-        queSize: usize = 0,                             // 尾指针，指向队尾 + 1
+        nums: []T = undefined, // 用于存储队列元素的数组
+        cap: usize = 0, // 队列容量
+        front: usize = 0, // 队首指针，指向队首元素
+        queSize: usize = 0, // 尾指针，指向队尾 + 1
         mem_arena: ?std.heap.ArenaAllocator = null,
-        mem_allocator: std.mem.Allocator = undefined,   // 内存分配器
+        mem_allocator: std.mem.Allocator = undefined, // 内存分配器
 
         // 构造函数（分配内存+初始化数组）
         pub fn init(self: *Self, allocator: std.mem.Allocator, cap: usize) !void {
@@ -27,7 +27,7 @@ pub fn ArrayQueue(comptime T: type) type {
             self.nums = try self.mem_allocator.alloc(T, self.cap);
             @memset(self.nums, @as(T, 0));
         }
-        
+
         // 析构函数（释放内存）
         pub fn deinit(self: *Self) void {
             if (self.mem_arena == null) return;
@@ -61,7 +61,7 @@ pub fn ArrayQueue(comptime T: type) type {
             // 在尾节点后添加 num
             self.nums[rear] = num;
             self.queSize += 1;
-        } 
+        }
 
         // 出队
         pub fn pop(self: *Self) T {
@@ -70,13 +70,13 @@ pub fn ArrayQueue(comptime T: type) type {
             self.front = (self.front + 1) % self.capacity();
             self.queSize -= 1;
             return num;
-        } 
+        }
 
         // 访问队首元素
         pub fn peek(self: *Self) T {
             if (self.isEmpty()) @panic("队列为空");
             return self.nums[self.front];
-        } 
+        }
 
         // 返回数组
         pub fn toArray(self: *Self) ![]T {
@@ -85,7 +85,10 @@ pub fn ArrayQueue(comptime T: type) type {
             @memset(res, @as(T, 0));
             var i: usize = 0;
             var j: usize = self.front;
-            while (i < self.size()) : ({ i += 1; j += 1; }) {
+            while (i < self.size()) : ({
+                i += 1;
+                j += 1;
+            }) {
                 res[i] = self.nums[j % self.capacity()];
             }
             return res;
@@ -96,7 +99,7 @@ pub fn ArrayQueue(comptime T: type) type {
 // Driver Code
 pub fn main() !void {
     // 初始化队列
-    var capacity: usize = 10;
+    const capacity: usize = 10;
     var queue = ArrayQueue(i32){};
     try queue.init(std.heap.page_allocator, capacity);
     defer queue.deinit();
@@ -111,20 +114,20 @@ pub fn main() !void {
     inc.PrintUtil.printArray(i32, try queue.toArray());
 
     // 访问队首元素
-    var peek = queue.peek();
+    const peek = queue.peek();
     std.debug.print("\n队首元素 peek = {}", .{peek});
 
     // 元素出队
-    var pop = queue.pop();
+    const pop = queue.pop();
     std.debug.print("\n出队元素 pop = {}，出队后 queue = ", .{pop});
     inc.PrintUtil.printArray(i32, try queue.toArray());
 
     // 获取队列的长度
-    var size = queue.size();
+    const size = queue.size();
     std.debug.print("\n队列长度 size = {}", .{size});
 
     // 判断队列是否为空
-    var is_empty = queue.isEmpty();
+    const is_empty = queue.isEmpty();
     std.debug.print("\n队列是否为空 = {}", .{is_empty});
 
     // 测试环形数组
